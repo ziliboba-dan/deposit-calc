377fc3b4dc696fa6d91089ab8117f23db58f41a2
danil <EvtushenkoAleksei@yandex.ru>
2017-02-19 14:54:14 +0700

    Implementation of user input

--- depocit.c
+++ depocit.c
@@ -2,5 +2,13 @@
 
 int main()
 {
+	int day, summa;
+	
+	printf("Enter the deposit amount: ");
+	scanf("%d", &summa);
+	
+	printf("Enter the term of the deposit: ");
+	scanf("%d", &day);
+
 	return 0;
 }
